dbd0db21288d14a32e718dbec8819ef262c353ed
Alexander Begoon <alexbassmusic@gmail.com>
2013-10-25 18:07:17 +0400

    Add 48px icon to the Toolbar. Add CSS to the default view. Rename menu item.

---
+++ administrator/views/magnets/tmpl/default.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @version     1.0.0
+ * @package     com_magnet
+ * @copyright   © 2013. All Rights Reserved.
+ * @license     GNU General Public License v2.0; See LICENSE.txt
+ * @author      Alexander.B <alexbassmusic@gmail.com> - https://www.odesk.com/users/~01ae8f6e1a81c189cf
+ */
+// no direct access
+defined('_JEXEC') or die;
+
+JHtml::_('behavior.tooltip');
+JHtml::_('behavior.formvalidation');
+JHtml::_('behavior.keepalive');
+// Import CSS
+$document = JFactory::getDocument();
+$document->addStyleSheet('components/com_magnet/assets/css/magnet.css');
+?>
